533de1a07f1128e78440170b54d12ceef80c3b62
Eduardo Nogueira <eduardodudunog@gmail.com>
2020-01-19 13:45:23 -0300

    Programa finalizado

---
+++ ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ZipCodeInformation
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddresAsync(string cep);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
 
 namespace ZipCodeInformation
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.Write("Informe seu CEP: ");
+
+                string CepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + CepInformado);
+
+                var address = await cepClient.GetAddresAsync(CepInformado);
+
+                Console.Write($"\nLogradouro: {address.Logradouro}\nBairro: {address.Bairro}\nCidade: {address.Localidade}" +
+                    $"\nUf: {address.Uf}");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro na consulta do CEP: " + e.Message);
+            }
         }
     }
 }
